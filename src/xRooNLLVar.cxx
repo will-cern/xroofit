6c6926c411edeb414b1bfe9d4d28fd73c53e86cb
will buttinger <will@cern.ch>
2022-09-16 14:37:56 +0100

    improve hypoPoint printout by including asimov point info, and fitresult names

--- src/xRooNLLVar.cxx
+++ src/xRooNLLVar.cxx
@@ -711,38 +711,50 @@ int xRooNLLVar::xRooHypoPoint::status() const {
 }
 
 void xRooNLLVar::xRooHypoPoint::Print() {
-    std::cout << "POI: " << poi().contentsString() << " , null: " << dynamic_cast<RooAbsReal*>(poi().first())->getVal() << " , alt: " << dynamic_cast<RooAbsReal*>(alt_poi().first())->getVal() << std::endl;
-    std::cout << "pllType: " << fPllType << " , ufit: ";
+    std::cout << "POI: " << poi().contentsString() << " , null: " << dynamic_cast<RooAbsReal*>(poi().first())->getVal() << " , alt: " << dynamic_cast<RooAbsReal*>(alt_poi().first())->getVal();
+    std::cout << " , pllType: " << fPllType << std::endl;
+
+    std::cout << " -      ufit: ";
     if(fUfit) {
-        std::cout << fUfit->minNll() << " (status=" << fUfit->status() << ") (mu_hat: " << mu_hat().getVal() << " +/- " << mu_hat().getError() << ")" << std::endl;
+        std::cout << fUfit->minNll() << " (status=" << fUfit->status() << ") (" << mu_hat().GetName() << "_hat: " << mu_hat().getVal() << " +/- " << mu_hat().getError() << ")" << std::endl;
     } else {
         std::cout << " Not calculated" << std::endl;
     }
-    std::cout << "null cfit: ";
+    std::cout << " - null cfit: ";
     if(fNull_cfit) {
-        std::cout << fNull_cfit->minNll() << " (status=" << fNull_cfit->status() << ")";
+        std::cout << fAlt_cfit->GetName() << " " << fNull_cfit->minNll() << " (status=" << fNull_cfit->status() << ")";
     } else {
-        std::cout << " Not calculated";
+        std::cout << "Not calculated";
     }
-    if (!std::isnan(dynamic_cast<RooAbsReal*>(poi().first())->getVal())) {
-        std::cout << " , alt cfit: ";
+    if (!std::isnan(dynamic_cast<RooAbsReal*>(alt_poi().first())->getVal())) {
+        std::cout << std::endl << " -  alt cfit: ";
         if (fAlt_cfit) {
-            std::cout << fAlt_cfit->minNll() << " (status=" << fAlt_cfit->status() << ")" << std::endl;
+            std::cout << fAlt_cfit->GetName() << " " << fAlt_cfit->minNll() << " (status=" << fAlt_cfit->status() << ")" << std::endl;
         } else {
-            std::cout << " Not calculated" << std::endl;
+            std::cout << "Not calculated" << std::endl;
         }
-        std::cout << "sigma_mu: ";
+        std::cout << " sigma_mu: ";
         if (!fAsimov || !fAsimov->fUfit || !fAsimov->fNull_cfit) {
-            std::cout << " Not calculated" << std::endl;
+            std::cout << "Not calculated";
         } else {
-            std::cout << sigma_mu().first << " +/- " << sigma_mu().second << std::endl;
+            std::cout << sigma_mu().first << " +/- " << sigma_mu().second;
         }
+        if (fAsimov) {
+            std::cout << std::endl;
+            std::cout << "   -      asimov ufit: ";
+            if(fAsimov->fUfit)  std::cout << fAsimov->fUfit->GetName() << " " <<  fAsimov->fUfit->minNll() << " (status=" << fAsimov->fUfit->status() << ")";
+            else std::cout << "Not calculated";
+            std::cout << std::endl <<  "   - asimov null cfit: ";
+            if(fAsimov->fNull_cfit)  std::cout << fAsimov->fNull_cfit->GetName() << " " << fAsimov->fNull_cfit->minNll() << " (status=" << fAsimov->fNull_cfit->status() << ")";
+            else std::cout << "Not calculated";
+        }
+        std::cout << std::endl;
     } else {
         std::cout << std::endl;
     }
-    if(fGenFit) std::cout << "genFit: " << fGenFit->GetName() << std::endl;
+    if(fGenFit) std::cout << " genFit: " << fGenFit->GetName() << std::endl;
     if (!nullToys.empty() || !altToys.empty()) {
-        std::cout << "null toys: " << nullToys.size();
+        std::cout << " null toys: " << nullToys.size();
         size_t firstToy = 0; while(firstToy < nullToys.size() && std::isnan(std::get<1>(nullToys[firstToy]))) firstToy++;
         if (firstToy>0) std::cout << " [ of which " << firstToy << " are bad]";
         std::cout << " , alt toys: " << altToys.size();
@@ -750,7 +762,7 @@ void xRooNLLVar::xRooHypoPoint::Print() {
         if (firstToy>0) std::cout << " [ of which " << firstToy << " are bad]";
         std::cout << std::endl;
     }
-    std::cout << "nllVar: " << nllVar << std::endl;
+    //std::cout << " nllVar: " << nllVar << std::endl;
 }
 
 RooRealVar& xRooNLLVar::xRooHypoPoint::mu_hat() {
@@ -768,7 +780,7 @@ std::shared_ptr<xRooNLLVar::xRooHypoPoint> xRooNLLVar::xRooHypoPoint::asimov(boo
         if(!nllVar->fFuncVars) nllVar->reinitialize();
         AutoRestorer snap(*nllVar->fFuncVars);
         auto theFit = (!data.first && fGenFit) ? fGenFit : cfit_alt(readOnly);
-        if (!theFit) return fAsimov;
+        if (!theFit || allowedStatusCodes.find(theFit->status())==allowedStatusCodes.end()) return fAsimov;
         *nllVar->fFuncVars = theFit->floatParsFinal();
         *nllVar->fFuncVars = theFit->constPars();
         auto asimov = nllVar->generate(true);
