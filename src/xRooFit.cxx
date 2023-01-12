c5916b7f11e2bd3ebbcce6d145752d1e33458384
will buttinger <will@cern.ch>
2023-01-12 15:23:48 +0000

    respond to comments from ROOT MR

--- src/xRooFit.cxx
+++ src/xRooFit.cxx
@@ -720,10 +720,10 @@ xRooFit::minimize(RooAbsReal &nll, const std::shared_ptr<ROOT::Fit::FitConfig> &
                      ->Result()
                      .Status(); // note: Minuit failure is status code 4, minuit2 that is edm above max
          minim = _minimizer.fitter()->Config().MinimizerType(); // may have changed value
-         statusHistory.push_back(std::make_pair(
+         statusHistory.emplace_back(
             _minimizer.fitter()->Config().MinimizerType() + _minimizer.fitter()->Config().MinimizerAlgoType() +
                std::to_string(_minimizer.fitter()->Config().MinimizerOptions().Strategy()),
-            status));
+            status);
          if (status % 1000 == 0)
             break; // fit was good
 
@@ -1122,8 +1122,9 @@ int xRooFit::minos(RooAbsReal &nll, const RooFitResult &ufit, const char *parNam
    par->setConstant(isConst);
 
    std::vector<std::pair<std::string, int>> statusHistory;
-   for (int i = 0; i < ufit.numStatusHistory(); i++)
+   for (int i = 0; i < ufit.numStatusHistory(); i++) {
       statusHistory.emplace_back(ufit.statusLabelHistory(i), ufit.statusCodeHistory(i));
+   }
    statusHistory.emplace_back(TString::Format("xMINOS_%s", parName), status);
    const_cast<RooFitResult &>(ufit).setStatusHistory(statusHistory);
    const_cast<RooFitResult &>(ufit).setStatus(ufit.status() + status);
