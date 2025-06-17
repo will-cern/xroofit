39a080f157b644bb8bdaaa4d83aa7159e7cedfa7
will buttinger <will@cern.ch>
2025-06-17 10:27:46 +0100

    fix bug in SaveAs

--- src/xRooNode.cxx
+++ src/xRooNode.cxx
@@ -11960,10 +11960,13 @@ void xRooNode::SaveAs(const char *filename, Option_t *option) const
                }
             }
          };
-         for(auto key : *gROOT->GetListOfKeys()) {
-            if (auto fitDb = gROOT->GetDirectory(key->GetName()); fitDb && TString(key->GetName()).BeginsWith("fitDatabase_")) {
-               CopyDir(fitDb, std::make_unique<TFile>(sFilename, "UPDATE").get());
-               Info("SaveAs", "Saved %s to %s", fitDb->GetName(), sFilename.Data());
+         if(gROOT->GetList()) {
+            for (auto key : *gROOT->GetList()) {
+               if (auto fitDb = dynamic_cast<TDirectory *>(key);
+                   fitDb && TString(key->GetName()).BeginsWith("fitDatabase_")) {
+                  CopyDir(fitDb, std::make_unique<TFile>(sFilename, "UPDATE").get());
+                  Info("SaveAs", "Saved %s to %s", fitDb->GetName(), sFilename.Data());
+               }
             }
          }
       } else {
