f73aed25e2e273668feb3f40a96f0c80732e491a
will.buttinger <will@cern.ch>
2026-05-07 17:18:24 +0100

    try forcing a flush

--- src/xRooFit.cxx
+++ src/xRooFit.cxx
@@ -546,6 +546,9 @@ ROOT::Math::IOptions *xRooFit::defaultFitConfigOptions()
 void printCerr(const char* msg) {
    if (Py_IsInitialized()) {
       PySys_WriteStderr("%s\n", msg);
+      if (PyObject* sys_stdout = PySys_GetObject("stderr"); sys_stdout != nullptr) {
+         Py_XDECREF(PyObject_CallMethod(sys_stdout, "flush", nullptr));
+      }
    } else {
       std::cerr << msg << std::endl;
    }
@@ -553,6 +556,9 @@ void printCerr(const char* msg) {
 void printCout(const char* msg) {
    if (Py_IsInitialized()) {
       PySys_WriteStdout("%s\n", msg);
+      if (PyObject* sys_stdout = PySys_GetObject("stdout"); sys_stdout != nullptr) {
+         Py_XDECREF(PyObject_CallMethod(sys_stdout, "flush", nullptr));
+      }
    } else {
       std::cout << msg << std::endl;
    }
